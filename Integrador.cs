2a7ee07911e0d61a59b4c480b3cbbdbafcbf568c
NorminGutierrez <amayabrandon164@gmail.com>
2022-06-08 20:50:08 -0600

    segundo commit

--- Integrador.cs
+++ Integrador.cs
@@ -1,2 +1,8 @@
 string nombre, apellido, mes, correo, contraseña, c_contraseña;
             int dia, año;
+            
+            Console.WriteLine("Digite su nombre");
+            nombre= Console.ReadLine();
+
+            Console.WriteLine("Digite su apellido");
+            apellido = Console.ReadLine();
